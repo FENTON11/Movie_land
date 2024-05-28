2b168e7cf0624be6a026a95c626401fefb5453d7
fenton <fencurt99@gmail.com>
2024-05-28 08:49:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import MovieCard from './Componets/MovieCard';
 import './App.css';
 
+
+const API_URL='http://www.omdbapi.com?apikey=8a577f3c';
+const movie1 ={
+  
+    "Title": "The Fast and the Furious",
+    "Year": "2001",
+    "imdbID": "tt0232500",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+
+}
 function App() {
+  const [movies, setMovies]= useState([]);
+  const [searchTerm ,setSearchTerm ]= useState('')
+  const searchMovies = async(title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    setMovies(data.Search)
+  
+  }
+  useEffect(()=>{
+   searchMovies('Fast and Furious')
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)}
+        />
+        <img
+        src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+        alt='search'
+        onClick={()=>searchMovies(searchTerm)}
+        />
+      </div>
+
+      {
+        movies?.length >0
+        ? (
+          <div className='container'>
+            {movies.map((movie)=>(
+              <MovieCard movie ={movie}/>
+            ))}
+         </div>
+         ) :(
+             <div className='empty'>
+              <h2>No movies found</h2>
+             </div>
+           )
+      }
     </div>
   );
 }
